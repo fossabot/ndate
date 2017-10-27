8b515157ce26589930b82f03f7e721c551736717
Andrei Kazakou <akazakou@exadel.com>
2017-10-27 15:03:25 +0300

    Update gulpfile.js to remove not used options

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const rimraf = require('gulp-rimraf');
 const tslint = require('gulp-tslint');
 const mocha = require('gulp-mocha');
 const shell = require('gulp-shell');
-const replace = require('gulp-replace');
 
 const outDir = 'build';
 
@@ -14,14 +13,8 @@ const outDir = 'build';
  * Remove build directory.
  */
 gulp.task('clean', () => {
-  return gulp.src(outDir, { read: false })
+  return gulp.src(`${outDir}/src/**/*`, { read: false })
     .pipe(rimraf());
-});
-
-gulp.task('coverage', () => {
-  return gulp.src([`${outDir}/src/**/*.js`, `!${outDir}/test/**/*.js`])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
 });
 
 /**
@@ -47,14 +40,14 @@ gulp.task('build', ['tslint', 'compile'], () => {
 /**
  * Run tests.
  */
-gulp.task('test', ['build', 'coverage'], () => {
+gulp.task('test', ['build'], () => {
 
   gulp.src([outDir + '/test/**/*.js'])
     .pipe(mocha())
-    .pipe(istanbul.writeReports({reportOpts: {dir: outDir + '/coverage'}}))
+    .pipe(istanbul.writeReports())
     .pipe(istanbul.enforceThresholds({
       thresholds: {
-        global: 90
+        global: 100
       }
     }))
     .once('error', (error) => {
@@ -65,17 +58,6 @@ gulp.task('test', ['build', 'coverage'], () => {
       process.exit();
     })
 
-});
-
-gulp.task('lcov', () => {
-  gulp.src(['build/coverage/lcov.info'])
-    .pipe(replace(`${__dirname}/`, ''))
-    .pipe(gulp.dest('build/coverage'));
-});
-
-gulp.task('copy', function() {
-  gulp.src(['coverage/lcov.info'])
-    .pipe(gulp.dest('build/coverage/'))
 });
 
 gulp.task('default', ['clean'], () => {
