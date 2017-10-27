011a14782d2dc11e5a04e2e1f00b7617715b6a5c
Andrei Kazakou <akazakou@exadel.com>
2017-10-27 10:49:38 +0300

    Remove unused interfaces from interval

--- src/Interfaces/Intervals.ts
+++ src/Interfaces/Intervals.ts
@@ -40,28 +40,6 @@ interface IBasicInterval {
   format(format: string): string;
 }
 
-/**
- * Absolute interval between two dates.
- *
- * For example difference between two dates like "2017-01-01T00:00:00.000Z" and "2018-01-01T00:00:00.000Z" will be:
- * years: 1, months: 12, days: 365, hours: 8760, minutes: 525600, seconds: 31536000, milliseconds: 31536000000
- */
-interface IAbsoluteInterval extends IBasicInterval {
-
-}
-
-/**
- * Relative interval between two dates.
- *
- * For example difference between two dates like "2017-01-01T00:00:00.000Z" and "2018-03-04T04:05:06.007Z" will be:
- * years: 1, months: 2, days: 3, hours: 4, minutes: 5, seconds: 6, milliseconds: 7
- */
-interface IRelativeInterval extends IBasicInterval {
-
-}
-
 export {
   IBasicInterval,
-  IAbsoluteInterval,
-  IRelativeInterval,
 }
