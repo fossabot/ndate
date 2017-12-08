35c0553fc2ad2ef4f02327e539a1ee52a9f809c3
Andrei Kazakou <akazakou@exadel.com>
2017-12-08 18:07:42 +0300

    Update NDate implementation to class definition

--- src/Interfaces/NDate.ts
+++ src/Interfaces/NDate.ts
@@ -1,36 +1,79 @@
 import {Absolute, Relative} from "../library/Interval";
 import {BasicDate} from "../library/BasicDate";
 
-interface NDate extends BasicDate {
+interface NDateConstructor {
+  /**
+   * Creates new NDate object without parameters
+   * @returns {NDate}
+   */
+  new(): NDateInterface;
+
+  /**
+   * Using milliseconds to create new NDate object
+   * @param {number} value
+   * @returns {NDate}
+   */
+  new(value: number): NDateInterface;
+
+  /**
+   * Using string to create new NDate object
+   * @param {string} value
+   * @returns {NDate}
+   */
+  new(value: string): NDateInterface;
+
+  /**
+   * Use Date or NDate object to create new NDate object
+   * @param {NDate | BasicDate | Date} value
+   * @returns {NDate}
+   */
+  new(value: NDateInterface | BasicDate | Date): NDateInterface;
+
+  /**
+   * Using legacy raw values for create NDate Object
+   * @param {number} year
+   * @param {number} month
+   * @param {number} date
+   * @param {number} hours
+   * @param {number} minutes
+   * @param {number} seconds
+   * @param {number} ms
+   * @returns {NDate}
+   */
+  new(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): NDateInterface;
+}
+
+interface NDateInterface extends BasicDate {
   /**
    * Add Absolute Interval object value to existed date object
    * @param {Absolute} interval Absolute Interval object value
-   * @returns {NDate} link to current object with updated date value
+   * @returns {NDateInterface} link to current object with updated date value
    */
-  add(interval: Absolute): NDate;
+  add(interval: Absolute): NDateInterface;
 
   /**
    * Add Relative Interval object value to existed date object
    * @param {Relative} interval Relative Interval object value
-   * @returns {NDate} link to current object with updated date value
+   * @returns {NDateInterface} link to current object with updated date value
    */
-  add(interval: Relative): NDate;
+  add(interval: Relative): NDateInterface;
 
   /**
    * Subtract Absolute Interval object value from current object value
    * @param {Absolute} interval Absolute Interval object that value should be subtracted from current NDate object value
-   * @returns {NDate} link to current object with updated date value
+   * @returns {NDateInterface} link to current object with updated date value
    */
-  sub(interval: Absolute): NDate;
+  sub(interval: Absolute): NDateInterface;
 
   /**
    * Subtract Relative Interval object value from current object value
    * @param {Relative} interval Relative Interval object that value should be subtracted from current NDate object value
-   * @returns {NDate} link to current object with updated date value
+   * @returns {NDateInterface} link to current object with updated date value
    */
-  sub(interval: Relative): NDate;
+  sub(interval: Relative): NDateInterface;
 }
 
 export {
-  NDate,
+  NDateInterface,
+  NDateConstructor,
 }
--- src/library/NDate/index.ts
+++ src/library/NDate/index.ts
@@ -1,68 +1,32 @@
 import NDateAdd from "./ndate.add";
 import NDateSub from "./ndate.sub";
 import {BasicDate} from "../BasicDate";
-import {Constructor} from "../../Interfaces/Constructor";
-import {NDate as INDate} from "../../Interfaces/NDate";
+import {NDateInterface, NDateConstructor} from "../../Interfaces/NDate";
 
 /**
  * Apply mixins of all available additional functionality
  * @see https://blog.mariusschulz.com/2017/05/26/typescript-2-2-mixin-classes
- * @type {Constructor}
+ * @type {NDateConstructor}
  */
-const NDatePrototype: Constructor = NDateSub(
+const NDatePrototype: NDateConstructor = NDateSub(
   NDateAdd(
     BasicDate
   )
 ) as any;
 
 /**
- * Implementation of function part for DateConstructor interface
+ * NDate class with all required methods implementation
  */
-const funcPart = function (year?: number | string, month?: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): Date | string {
-  // called as function
-  if (typeof new.target === 'undefined') {
-    return Date();
-  }
-
-  // called as constructor with no arguments
-  if (typeof year === 'undefined') {
-    return new NDatePrototype();
-  }
-
-  // called as constructor with string value argument
-  if (typeof year === 'string') {
-    return new NDatePrototype(year);
-  }
-
-  // called as constructor with number value argument
-  if (typeof month === 'undefined') {
-    return new NDatePrototype(year);
-  }
-
-  // called as constructor with year, month, date, etc arguments:
-  return new NDatePrototype(year, month, date, hours, minutes, seconds, ms);
-} as Constructor;
-
-/**
- * That definition means the class constructor is also a callable function with no arguments that returns a string when
- * called without new. You can't use a ES2015-or-later class and adhere to the specification, since it would need to
- * throw a TypeError upon being called without new. Instead you can return a function that detects being called with
- * new, with extra properties added to implement static methods.
- */
-const NDate: Constructor = Object.assign(funcPart, {
-  /**
-   * DateConstructor instance by merging the functional part with something that implements static methods
-   */
-  prototype: NDatePrototype.prototype as INDate,
+class NDate extends NDatePrototype implements NDateInterface {
   /**
    * Parses a string containing a date, and returns the number of milliseconds between that date and midnight, January 1, 1970.
    * @param s A date string
    * @param {string} s
    * @returns {number}
    */
-  parse(s: string): number {
+  public static parse(s: string): number {
     return Date.parse(s);
-  },
+  };
 
   /**
    * Returns the number of milliseconds between midnight, January 1, 1970 Universal Coordinated Time (UTC) (or GMT) and the specified date.
@@ -76,18 +40,18 @@ const NDate: Constructor = Object.assign(funcPart, {
    * @returns {number}
    * @constructor
    */
-  UTC(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): number {
+  static UTC(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number): number {
     return Date.UTC(year, month, date, hours, minutes, seconds, ms);
-  },
+  };
 
   /**
    * Returns the number of milliseconds between midnight, January 1, 1970 Universal Coordinated Time (UTC) (or GMT) and the specified date.
    * @returns {number}
    */
-  now(): number {
+  static now(): number {
     return Date.now();
-  }
-});
+  };
+}
 
 export {
   NDate as Date,
